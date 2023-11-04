90be478da20af68856ffd62a86677c34843e24e9
elijahcw-git <walkerelijahc@gmail.com>
2023-11-04 13:27:22 -0500

    initial commit

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+with open('nb_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    prediction_text = ''
+    if request.method == 'POST':
+        features = [float(x) for x in request.form.values()]
+        prediction = model.predict([features])
+        prediction_text = "Malignant" if prediction[0] == 4 else "Benign"
+    return render_template('index.html', prediction_text=prediction_text)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
